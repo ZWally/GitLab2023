00e5fdd5b9651197e3ece103b9560c42ef73a419
SmileyJack <jacksmiley26@gmail.com>
2023-02-03 12:24:02 -0600

    Changed it + DaBaby

--- src/GitLab.java
+++ src/GitLab.java
@@ -4,6 +4,9 @@ public class GitLab {
     public static void main(String[] args) {
         System.out.println("This is a lesson on git");
 
+        GitLabHelper helper = new GitLabHelper("Zach", "Jack");
+        helper.PrintWelcomeMessage();
+
     }
 
 }
